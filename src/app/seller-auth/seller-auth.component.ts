8e939bd6186e15ac99b1a457947f1d050e2a47da
Behnam <behnam.vosoogh@outlook.com>
2024-07-29 11:51:55 -0400

    sign up form created

--- src/app/seller-auth/seller-auth.component.ts
+++ src/app/seller-auth/seller-auth.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-seller-auth',
   standalone: true,
-  imports: [],
+  imports: [FormsModule], // Import FormsModule here
   templateUrl: './seller-auth.component.html',
-  styleUrl: './seller-auth.component.css'
+  styleUrl: './seller-auth.component.css',
 })
 export class SellerAuthComponent {
-
+  signUp(data: object): void {
+    console.warn('Form Data:', data);
+  }
 }
