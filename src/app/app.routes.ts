7d984699a2b1efcb685c741eec112d0e956f6111
Behnam <behnam.vosoogh@outlook.com>
2024-07-29 11:28:59 -0400

    404 page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'seller-auth', component: SellerAuthComponent },
+  { path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page
 ];
