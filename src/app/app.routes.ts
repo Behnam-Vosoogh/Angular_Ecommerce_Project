8768919e0cb4cbc4ee603273dd99bf8beed37d0e
Behnam <behnam.vosoogh@outlook.com>
2024-08-09 23:20:18 -0400

    rout added to auth gurd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { HomeComponent } from './home/home.component';
 import { SellerAuthComponent } from './seller-auth/seller-auth.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { SellerHomeComponent } from './seller-home/seller-home.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'seller-auth', component: SellerAuthComponent },
-  { path: 'seller-home', component: SellerHomeComponent },
+  {
+    path: 'seller-home',
+    component: SellerHomeComponent,
+    canActivate: [authGuard],
+  },
   { path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page
 ];
