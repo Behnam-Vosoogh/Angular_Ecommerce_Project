e4ab52788d750a34bd9208c9170cbd8a4ae21c65
Behnam <behnam.vosoogh@outlook.com>
2024-09-02 12:30:53 -0400

    Footer added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { HttpClient } from '@angular/common/http';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent], // Remove HttpClientModule
+  imports: [RouterOutlet, HeaderComponent, FooterComponent], // Remove HttpClientModule
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'], // Fix typo: should be styleUrls with 's'
 })
